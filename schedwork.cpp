fa03405b9b69acb04de92b5d21f6b3e302e97fb2
Fiona Zara <fzara@usc.edu>
2025-11-28 06:04:21 +0000

    submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,21 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+void scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+    map<size_t, size_t>& numShifts, 
+    size_t n, size_t d, int& bs
+);
 
+bool isValid(
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    map<size_t, size_t>& numShifts, 
+    size_t n, size_t k
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +46,89 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    int badShift=-1;
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
+    for(size_t n=0; n<avail.size(); n++)
+    {
+        vector<Worker_T> row;
+        for(size_t d=0; d<dailyNeed; d++)
+            row.push_back(INVALID_ID);
+        sched.push_back(row);
+    }
+    map<size_t, size_t> numShifts;
+    scheduleHelper(avail, dailyNeed, maxShifts, sched, numShifts, 0, 0, badShift);
 
-
-
-
+    // cout << "printing sched" << endl;
+    // for(int i=0; i<sched.size(); i++)
+    // {
+    //     for(int j=0; j<sched[i].size(); j++)
+    //     {
+    //         cout << sched[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << "printing avail" << endl;
+    // for(int i=0; i<avail.size(); i++)
+    // {
+    //     for(int j=0; j<avail[i].size(); j++)
+    //     {
+    //         cout << avail[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // vector<int> inner = *find(sched.begin(), sched.end(), INVALID_ID);
+    // if(inner == sched.end()) return false;
+    // return ( *find(inner.begin(), inner.end(), INVALID_ID)!= INVALID_ID);
+    // vector<int> inner = *find(sched.begin(), sched.end(), INVALID_ID);
+    // if(inner == sched.end()) return false;
+    
+    // return ( find(sched.begin(), sched.end(), INVALID_ID)!= sched.end());
+    return (badShift==-1);
+    
 }
 
+void scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, 
+    map<size_t, size_t>& numShifts, 
+    size_t n, size_t d, int& bs
+)
+{
+    
+    if(d>=dailyNeed)
+    {
+        d=0;
+        n++;
+    }
+    if(n>=avail.size()) return;
+
+    for(size_t k=0; k<avail[n].size(); k++)
+    {
+        numShifts[k] = numShifts[k] + 1;
+        // cout << "num shifts is "<< (*numShifts.find(k)).second << endl;
+        sched[n][d] = k;
+        if(isValid(avail, maxShifts, numShifts, n, k)) 
+            return scheduleHelper(avail, dailyNeed, maxShifts, sched, numShifts, n, (d+1), bs);
+        numShifts[k] = numShifts[k] - 1;
+    }
+
+    sched[n][d] = INVALID_ID;
+    bs = d;
+}
+
+bool isValid(
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    map<size_t, size_t>& numShifts, 
+    size_t n, size_t k  
+)
+{
+    return avail[n][k] && ((*(numShifts.find(k))).second <= maxShifts);
+
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool isValid(string in, string floating, string potential);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+		set<string> results;
+		set<string>::iterator it; 
+		// for all words in the dict, add if valid
+		for(it=dict.begin(); it!=dict.end(); ++it)
+		{
+			// check if right length
+			if((*it).size()!=in.size()) continue;
+			if(isValid(in, floating, *it)) results.insert(*it);
+		}
 
+		return results;
 }
 
 // Define any helper functions here
+bool isValid(string in, string floating, string potential)
+{
+	// essentially maps characters in floating to an array
+	// first index is the # times it appears in potential
+	//  second index is the # times it should be in potential
+	map<char, int[2]> yellows;
+	
+	// setting up initial array for all keys
+	for(unsigned int i= 0; i<floating.size(); i++)
+	{
+		yellows[floating[i]][0] = 0;
+		if (yellows[floating[i]][1] != 0) 
+			yellows[floating[i]][1] = yellows[floating[i]][1]+1;
+		else 
+			yellows[floating[i]][1] = 1;
+	}
+
+	for(unsigned int i=0; i<potential.size(); i++)
+	{
+		// checking for uppercase
+		if(potential[i]>='A' && potential[i]<='Z') return false;
+		// green letter is not in word
+		if(in[i]!='-' && potential[i]!=in[i]) return false;
+		// found a yellow letter, updating first index of array
+		if(floating.find(potential[i])!=-1)
+				yellows[potential[i]][0] = yellows[potential[i]][0] + 1;
+	}
+
+	// checking if yellows showed up the right number of times
+	map<char, int[2]>::iterator it;
+	for(it=yellows.begin(); it!=yellows.end(); ++it)
+		if((*it).second[0]<(*it).second[1]) return false;
+	
+	return true;
+}
